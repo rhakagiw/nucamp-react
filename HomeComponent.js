86a23056a1305e9b487bc3c109a6a65fa59b1066
Rhakagi <rhakagiw@gmail.com>
2020-07-23 10:54:30 -0400

    first commit

---
+++ HomeComponent.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Nav, Navbar, Navbarbrand, NavbarToggler,Collapse,NavItem,Jumbotron} from'reactstrap';
+import{NavLink} from 'react-router-dom';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+function Home(props) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md m-1">
+                    <RenderCard item={props.campsite} />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.partner} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
+export default Home;
